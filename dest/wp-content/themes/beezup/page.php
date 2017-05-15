df1f745b811a5fc49382ba44ae53d5a22a7e1853
Elisabeth Hamel <elisabeth@stereosuper.fr>
2017-05-15 10:55:14 +0200

    :art: bloc-full

--- dest/wp-content/themes/beezup/page.php
+++ dest/wp-content/themes/beezup/page.php
@@ -1,21 +1,36 @@
 <?php get_header(); ?>
 
-<div class='container-small'>
-	<?php if ( have_posts() ) : the_post(); ?>
-
+<?php if ( have_posts() ) : the_post(); ?>
+	
+	<div class='container-small'>
 		<h1>
 			<?php the_title(); ?>
-			<?php if(get_field('title2')){ ?>
+			<?php if( get_field('title2') ){ ?>
 				<span><?php the_field('title2'); ?></span>
 			<?php } ?>
 		</h1>
-		<?php the_content(); ?>
+	</div>
+		
+	<?php
+		if( have_rows('content') ){
+			while ( have_rows('content') ){ the_row();
+				if( get_row_layout() == 'wysiwyg' ){ ?>
+					<div class='container-small'><?php the_sub_field('wysiwyg'); ?></div>
+				<?php }elseif( get_row_layout() == 'blockFull' ){ ?>
+					<div class='block-full'>
+						<div class='container-small'><?php the_sub_field('blockFull'); ?></div>
+					</div>
+				<?php }
+			}
+		}
+	?>
 
-	<?php else : ?>
+<?php else : ?>
 
+	<div class='container-small'>
 		<h1>404</h1>
+	</div>
 
-	<?php endif; ?>
-</div>
+<?php endif; ?>
 
 <?php get_footer(); ?>
--- dest/wp-content/themes/beezup/single.php
+++ dest/wp-content/themes/beezup/single.php
@@ -1,34 +1,36 @@
 <?php get_header(); ?>
 
-	<?php if ( have_posts() ) : ?>
+<?php if ( have_posts() ) : the_post(); ?>
+	
+	<div class='container-small'>
+		<h1>
+			<?php the_title(); ?>
+			<?php if( get_field('title2') ){ ?>
+				<span><?php the_field('title2'); ?></span>
+			<?php } ?>
+		</h1>
+	</div>
+		
+	<?php
+		if( have_rows('content') ){
+			while ( have_rows('content') ){ the_row();
+				if( get_row_layout() == 'wysiwyg' ){ ?>
+					<div class='container-small'><?php the_sub_field('wysiwyg'); ?></div>
+				<?php }elseif( get_row_layout() == 'blockFull' ){ ?>
+					<div class='block-full'>
+						<div class='container-small'><?php the_sub_field('blockFull'); ?></div>
+					</div>
+				<?php }
+			}
+		}
+	?>
 
-		<?php while ( have_posts() ) : the_post(); ?>
+<?php else : ?>
 
-			<article>
-					
-				<h1>
-					<?php the_title(); ?>
-					<?php if(get_field('title2')){ ?>
-						<span><?php the_field('title2'); ?></span>
-					<?php } ?>
-				</h1>
-				<div>
-					<?php echo get_the_date(); ?>
-				</div>
-						
-				<?php the_content(); ?>
-						
-			</article>
+	<div class='container-small'>
+		<h1>404</h1>
+	</div>
 
-		<?php endwhile; ?>
-
-
-	<?php else : ?>
-				
-		<article>
-			<h1>404</h1>
-		</article>
-
-	<?php endif; ?>
+<?php endif; ?>
 
 <?php get_footer(); ?>
