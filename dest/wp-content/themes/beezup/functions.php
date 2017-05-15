17ce0f66de91125ddab70b1dfac51e19cbd41c4d
Elisabeth Hamel <elisabeth@stereosuper.fr>
2017-05-15 12:02:04 +0200

    :art: new format + colors wysiwyg

--- dest/wp-content/themes/beezup/functions.php
+++ dest/wp-content/themes/beezup/functions.php
@@ -50,7 +50,7 @@ remove_action( 'wp_head', 'wp_oembed_add_discovery_links' );
 // remove comment author class
 function beezup_remove_comment_author_class( $classes ){
 	foreach( $classes as $key => $class ){
-		if(strstr($class, 'comment-author-')) unset( $classes[$key] );
+		if( strstr($class, 'comment-author-') ) unset( $classes[$key] );
 	}
 	return $classes;
 }
@@ -64,8 +64,8 @@ add_filter( 'login_errors', create_function('$a', "return null;") );
 /* Admin
 /*-----------------------------------------------------------------------------------*/
 // Remove some useless admin stuff
-function beezup_remove_submenus() {
-  $page = remove_submenu_page( 'themes.php', 'themes.php' );
+function beezup_remove_submenus(){
+    remove_submenu_page( 'themes.php', 'themes.php' );
 }
 add_action( 'admin_menu', 'beezup_remove_submenus', 999 );
 function beezup_remove_top_menus( $wp_admin_bar ){
@@ -75,13 +75,13 @@ add_action( 'admin_bar_menu', 'beezup_remove_top_menus', 999 );
 
 // Enlever le lien par défaut autour des images
 function beezup_imagelink_setup(){
-	if(get_option( 'image_default_link_type' ) !== 'none') update_option('image_default_link_type', 'none');
+	if( get_option( 'image_default_link_type' ) !== 'none' ) update_option('image_default_link_type', 'none');
 }
 add_action( 'admin_init', 'beezup_imagelink_setup' );
 
 // Enlever les <p> autour des images
 function beezup_remove_p_around_images($content){
-   return preg_replace('/<p>\s*(<a .*>)?\s*(<img .* \/>)\s*(<\/a>)?\s*<\/p>/iU', '\1\2\3', $content);
+    return preg_replace( '/<p>\s*(<a .*>)?\s*(<img .* \/>)\s*(<\/a>)?\s*<\/p>/iU', '\1\2\3', $content );
 }
 add_filter( 'the_content', 'beezup_remove_p_around_images' );
 
@@ -94,10 +94,10 @@ add_filter( 'upload_mimes', 'beezup_mime_types' );
 
 // Custom posts in the dashboard
 function beezup_right_now_custom_post() {
-    $post_types = get_post_types(array( '_builtin' => false ) , 'objects' , 'and');
-    foreach($post_types as $post_type){
+    $post_types = get_post_types( array( '_builtin' => false ) , 'objects' , 'and' );
+    foreach( $post_types as $post_type ){
         $cpt_name = $post_type->name;
-        if($cpt_name !== 'acf-field-group' && $cpt_name !== 'acf-field' && $cpt_name !== 'omapi'){
+        if( $cpt_name !== 'acf-field-group' && $cpt_name !== 'acf-field' && $cpt_name !== 'omapi' ){
             $num_posts = wp_count_posts($post_type->name);
             $num = number_format_i18n($num_posts->publish);
             $text = _n($post_type->labels->name, $post_type->labels->name , intval($num_posts->publish));
@@ -107,22 +107,29 @@ function beezup_right_now_custom_post() {
 }
 add_action( 'dashboard_glance_items', 'beezup_right_now_custom_post' );
 
+// Add new styles to wysiwyg
 function beezup_button($buttons){
-   array_unshift($buttons, 'styleselect');
-   return $buttons;
+    array_unshift( $buttons, 'styleselect' );
+    return $buttons;
 }
-add_filter('mce_buttons_2', 'beezup_button');
+add_filter( 'mce_buttons_2', 'beezup_button' );
 function beezup_init_editor_styles(){
-   add_editor_style();
+    add_editor_style();
 }
-add_action('after_setup_theme', 'beezup_init_editor_styles');
+add_action( 'after_setup_theme', 'beezup_init_editor_styles' );
+
 // Customize a bit the wysiwyg editor
 function beezup_mce_before_init( $styles ){
     $style_formats = array(
         array(
-            'title' => 'Star',
+            'title' => 'Bloc étoile',
             'selector' => 'blockquote',
             'classes' => 'star'
+        ),
+        array(
+            'title' => 'Bloc bleu',
+            'selector' => 'p',
+            'classes' => 'block-full'
         ),
         array(
             'title' => 'Image pleine largeur',
@@ -139,7 +146,7 @@ function beezup_mce_before_init( $styles ){
     // Remove h1 and code
     $styles['block_formats'] = 'Paragraph=p;Heading 2=h2;Heading 3=h3;Heading 4=h4;Heading 5=h5;Heading 6=h6';
     // Let only the colors you want
-    $styles['textcolor_map'] = '[' . "'000000', 'Noir', '565656', 'Texte'" . ']';
+    $styles['textcolor_map'] = '[' . "'000000', 'Noir', 'ffffff', 'Blanc', '262729', 'Texte', '009ae1', 'Principale', 'e6006c', 'Secondaire'" . ']';
     return $styles;
 }
 add_filter( 'tiny_mce_before_init', 'beezup_mce_before_init' );
@@ -147,15 +154,13 @@ add_filter( 'tiny_mce_before_init', 'beezup_mce_before_init' );
 /*-----------------------------------------------------------------------------------*/
 /* Menus
 /*-----------------------------------------------------------------------------------*/
-register_nav_menus(
-	array(
-		'primary' => 'Primary Menu',
-	)
-);
+register_nav_menus( array(
+    'primary' => 'Primary Menu',
+) );
 
 // Cleanup WP Menu html
 function beezup_css_attributes_filter($var){
-    return is_array($var) ? array_intersect($var, array('current-menu-item', 'current_page_parent')) : '';
+    return is_array( $var ) ? array_intersect( $var, array('current-menu-item', 'current_page_parent') ) : '';
 }
 add_filter( 'nav_menu_css_class', 'beezup_css_attributes_filter' );
 
@@ -164,7 +169,7 @@ add_filter( 'nav_menu_css_class', 'beezup_css_attributes_filter' );
 /* Sidebar & Widgets
 /*-----------------------------------------------------------------------------------*/
 function beezup_register_sidebars(){
-	register_sidebar(array(
+	register_sidebar( array(
 		'id' => 'sidebar',
 		'name' => 'Sidebar',
 		'description' => 'Take it on the side...',
@@ -173,25 +178,25 @@ function beezup_register_sidebars(){
 		'before_title' => '',
 		'after_title' => '',
 		'empty_title'=> ''
-	));
+	) );
 }
 add_action( 'widgets_init', 'beezup_register_sidebars' );
 
 // Deregister default widgets
 function beezup_unregister_default_widgets(){
-    unregister_widget('WP_Widget_Pages');
-    unregister_widget('WP_Widget_Calendar');
-    unregister_widget('WP_Widget_Archives');
-    unregister_widget('WP_Widget_Links');
-    unregister_widget('WP_Widget_Meta');
-    unregister_widget('WP_Widget_Search');
-    unregister_widget('WP_Widget_Text');
-    unregister_widget('WP_Widget_Categories');
-    unregister_widget('WP_Widget_Recent_Posts');
-    unregister_widget('WP_Widget_Recent_Comments');
-    unregister_widget('WP_Widget_RSS');
-    unregister_widget('WP_Widget_Tag_Cloud');
-    unregister_widget('WP_Nav_Menu_Widget');
+    unregister_widget( 'WP_Widget_Pages' );
+    unregister_widget( 'WP_Widget_Calendar' );
+    unregister_widget( 'WP_Widget_Archives' );
+    unregister_widget( 'WP_Widget_Links' );
+    unregister_widget( 'WP_Widget_Meta' );
+    unregister_widget( 'WP_Widget_Search' );
+    unregister_widget( 'WP_Widget_Text' );
+    unregister_widget( 'WP_Widget_Categories' );
+    unregister_widget( 'WP_Widget_Recent_Posts' );
+    unregister_widget( 'WP_Widget_Recent_Comments' );
+    unregister_widget( 'WP_Widget_RSS' );
+    unregister_widget( 'WP_Widget_Tag_Cloud' );
+    unregister_widget( 'WP_Nav_Menu_Widget' );
 }
 add_action( 'widgets_init', 'beezup_unregister_default_widgets' );
 
@@ -205,7 +210,7 @@ function beezup_defer_attr($tag, $handle){
    
     foreach( $scriptsToDefer as $script ){
         if( $script !== $handle ){
-            return str_replace(' src', ' defer src', $tag);
+            return str_replace( ' src', ' defer src', $tag );
         }
     }
     return $tag;
