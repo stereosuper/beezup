3e1cd1a351efc71da8287ad2965f52ccb4e622d2
Elisabeth Hamel <elisabeth@stereosuper.fr>
2017-05-15 11:14:23 +0200

    :art: cleanup footer

--- dest/wp-content/themes/beezup/functions.php
+++ dest/wp-content/themes/beezup/functions.php
@@ -211,4 +211,16 @@ function beezup_scripts(){
 }
 add_action( 'wp_enqueue_scripts', 'beezup_scripts' );
 
+function beezup_footer(){ ?>
+    <!-- SendinBlue Ajax Form validation -->
+    <script>
+        var sib_prefix = 'sib', sib_dateformat = 'dd-mm-yyyy';
+    </script>
+    <script src='https://my.sendinblue.com/public/theme/version4/assets/js/src/subscribe-validate.js?v=1489660964' defer></script>
+
+    <!-- Appointlet popin -->
+    <script src='https://d35xd5ovpwtfyi.cloudfront.net/loader/loader.min.js' defer></script>
+<?php }
+add_action( 'wp_footer', 'beezup_footer', 20);
+
 ?>
