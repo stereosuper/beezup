5c6e421717c77c56e33a4aa6bcc4f3c6699d5842
Elisabeth Hamel <elisabeth@stereosuper.fr>
2017-05-18 11:13:59 +0200

    :art: update reseaux template

--- src/theme/reseaux.php
+++ src/theme/reseaux.php
@@ -8,11 +8,11 @@ $apiUrl = 'https://api.beezup.com/v2/public/';
 $country = isset( $_GET['country'] ) ? $_GET['country'] : '';
 $currentUrl = $country ? 'channels/' . $country : 'channels/USA';
 
-$channelsIndex = json_decode( wp_remote_get($apiUrl . 'lov/www_ChannelCountry')['body'] );
-$currentChannel = json_decode( wp_remote_get($apiUrl . $currentUrl)['body'] );
+$dataChannelsIndex = wp_remote_get( $apiUrl . 'lov/www_ChannelCountry' );
+$dataCurrentChannel = wp_remote_get( $apiUrl . $currentUrl );
 
-
-// print_r(json_decode( wp_remote_get($apiUrl . 'lov')['body'] ));
+$channelsIndex = $dataChannelsIndex['response']['code'] === 200 ? json_decode( $dataChannelsIndex['body'] ) : false;
+$currentChannel = $dataCurrentChannel['response']['code'] === 200 ? json_decode( $dataCurrentChannel['body'] ) : false;
 
 
 get_header(); ?>
@@ -37,32 +37,38 @@ get_header(); ?>
 
         <?php the_post_thumbnail( 'full' ); ?>
 
-        <form action='<?php the_permalink(); ?>' method='GET'>
-            <select name='country'>
-                <?php foreach( $channelsIndex->items as $channel ){ ?>
-                    <?php $code = $channel->codeIdentifier; ?>
-                    <option value='<?php echo $code; ?>' <?php if($code === $country){ echo 'selected'; } ?>>
-                        <?php echo $channel->translationText; ?>
-                    </option>
-                <?php } ?>
-            </select>
+        <?php if( $channelsIndex ){ ?>
+            <form action='<?php the_permalink(); ?>' method='GET'>
+                <select name='country'>
+                    <?php foreach( $channelsIndex->items as $channel ){ ?>
+                        <?php $code = $channel->codeIdentifier; ?>
+                        <option value='<?php echo $code; ?>' <?php if($code === $country){ echo 'selected'; } ?>>
+                            <?php echo $channel->translationText; ?>
+                        </option>
+                    <?php } ?>
+                </select>
 
-            <button type='submit' name='filter' value='true'>Go</button>
-        </form>
+                <button type='submit' name='filter' value='true'>Go</button>
+            </form>
+        <?php } ?>
 	</section>
 
     <section class='container'>
-        <ul>
-            <?php foreach($currentChannel->channels as $partner){ ?>
-                <?php $name = $partner->name; ?>
-                <li>
-                    <a href='<?php echo $partner->homeUrl; ?>' title='<?php echo $name; ?>' target='_blank'>
-                        <?php echo $name; ?>
-                        <img src='<?php echo $partner->logoUrl; ?>' alt='<?php echo $name; ?>'>
-                    </a>
-                </li>
-            <?php } ?>
-        </ul>
+        <?php if( $currentChannel ){ ?>
+            <ul>
+                <?php foreach($currentChannel->channels as $partner){ ?>
+                    <?php $name = $partner->name; ?>
+                    <li>
+                        <a href='<?php echo $partner->homeUrl; ?>' title='<?php echo $name; ?>' target='_blank'>
+                            <?php echo $name; ?>
+                            <img src='<?php echo $partner->logoUrl; ?>' alt='<?php echo $name; ?>'>
+                        </a>
+                    </li>
+                <?php } ?>
+            </ul>
+        <?php }else{ ?>
+            <p><?php _e("Channels can't be displayed at this time. Please come back later!", 'beezup'); ?></p>
+        <?php } ?>
     </section>
 
     <?php get_template_part('includes/free-links'); ?>
