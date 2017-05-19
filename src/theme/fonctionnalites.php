93cc269bed4b17efdba369271811910a65c52f26
ClementLmn <clementlemoine.cl@gmail.com>
2017-05-19 17:31:46 +0200

    :art: style fonctionnalites

--- src/theme/fonctionnalites.php
+++ src/theme/fonctionnalites.php
@@ -47,37 +47,59 @@ get_header(); ?>
         </div>
     </section>
 
+    <?php if( have_rows('sections') ){ ?>
+    <?php $i = 1; ?>
+        <nav id='sideLinksNav' class='side-links'>
+            <ul>
+                <?php while( have_rows('sections') ){ the_row(); ?>
+                    <li>
+                        <a href='#<?php the_sub_field('anchor'); ?>' title='<?php the_sub_field('title'); ?>' ><?php echo sprintf('%02d', $i); ?></a>
+                    </li>
+                <?php $i++; } ?>
+            </ul>
+        </nav>
+    <?php } ?>
+
     <div class='container'>
         <?php if( have_rows('sections') ){ ?>
-            <?php $i = 01; ?>
+            <?php $i = 1; $j = 1; ?>
             <?php while( have_rows('sections') ){ the_row(); ?>
                 
                 <section id='<?php the_sub_field('anchor'); ?>'>
                     <?php if( get_sub_field('title') ){ ?>
-                        <h2 class='h1'><span class='number-index'><?php echo sprintf('%02d', $i) ?></span><p><?php the_sub_field('title'); ?></p></h2>
+                        <h2 class='h1'><span class='number-index'><?php echo sprintf('%02d', $i); ?></span><p><?php the_sub_field('title'); ?></p></h2>
                     <?php } ?>
                     
                     <?php if( have_rows('subSections') ){ ?>
                         <?php while( have_rows('subSections') ){ the_row(); ?>
-                            <?php if( get_sub_field('title') ){ ?>
-                                <h3 class='h2'><?php the_sub_field('title'); ?></h3>
-                            <?php } ?>
-                            
-                            <?php the_sub_field('text'); ?>
-                            
-                            <?php if( get_sub_field('star') ){ ?>
-                                <div class='star'><?php the_sub_field('star'); ?></div>
-                            <?php } ?>
-                            
-                            <?php echo wp_get_attachment_image( get_sub_field('img'), 'full' ); ?>
-                            
-                            <?php if( get_sub_field('link') && get_sub_field('linkText') ){ ?>
-                                <a href='<?php the_sub_field('link'); ?>' class='link-arrow' title='<?php the_sub_field('linkText'); ?>'><?php the_sub_field('linkText'); ?></a>
-                            <?php } ?>
-                        <?php } ?>
+                            <div class='subsection <?php echo ($j%2 !== 0 ? 'odd' : 'even') ?>'>
+                                <div class='subsection-content'>
+                                    <div class='subsection-text'>
+                                        <?php if( get_sub_field('title') ){ ?>
+                                            <h3 class='h2'><?php the_sub_field('title'); ?></h3>
+                                        <?php } ?>
+                                        
+                                        <?php the_sub_field('text'); ?>
+                                        
+                                        <?php if( get_sub_field('star') ){ ?>
+                                            <div class='star'><?php the_sub_field('star'); ?></div>
+                                        <?php } ?>
+                                    </div>
+                                    <div class='subsection-illu'>
+                                        <?php echo wp_get_attachment_image( get_sub_field('img'), 'full' ); ?>
+                                    </div>
+                                </div>
+
+                                <?php if( get_sub_field('link') && get_sub_field('linkText') ){ ?>
+                                    <div class='subsection-link'>
+                                        <a href='<?php the_sub_field('link'); ?>' class='link-arrow' title='<?php the_sub_field('linkText'); ?>'><?php the_sub_field('linkText'); ?></a>
+                                    </div>
+                                <?php } ?>
+                            </div>
+                        <?php $j++;} ?>
                     <?php } ?>
                 </section>
-
+                <hr/>
             <?php $i++; } ?>
         <?php } ?>
 
