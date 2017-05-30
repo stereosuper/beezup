9783eb7233f3d01ffdc39337357826b7bcdb87eb
Elisabeth Hamel <elisabeth@stereosuper.fr>
2017-05-30 14:52:27 +0200

    :art: some js improvements

--- src/js/scrollTo.js
+++ src/js/scrollTo.js
@@ -10,48 +10,47 @@ var throttle = require('./throttle.js');
 module.exports = function(elt, activeClass = false){
     if (!elt.length) return;
     
-    var target, scrollPos, currLink, refElement;
+    var target, scrollPos, currLink, refElement, link;
 
-    function scrollTo(e) {
+    function scrollTo(e){
         e.preventDefault();
-        var link = e.target.hash;
-        TweenLite.to(window, 1, {scrollTo: { y: $(link).offset().top - 70 }, onComplete: function () {
+        link = e.target.hash;
+        TweenLite.to(window, 1, {scrollTo: { y: $(link).offset().top - 70 }, onComplete: function(){
             history.pushState(null, null, link);
         }});
     }
 
-    function onScroll(){
-        if (!activeClass) return;
-        scrollPos = $(document).scrollTop();
-        elt.find('a[href^="#"]').each(function () {
-            currLink = $(this);
-            refElement = $(currLink.attr("href"));
-            if (refElement.data('top')-100 <= scrollPos && refElement.data('top')+ refElement.data('height')+100 > scrollPos) {
-                elt.find('li').removeClass("active");
-                currLink.parent().addClass("active");
-            }
-            else{
-                currLink.parent().removeClass("active");
-            }
-        });
-    }
-
-    function onResize() {
-        elt.find('a[href^="#"]').each(function () {
+    function setData(){
+        elt.find('a[href^="#"]').each(function(){
             target = $($(this).attr('href'));
             target.data('top', target.offset().top).data('height', target.height());
         });
     }
 
-    elt.on('click', 'a', scrollTo).find('a[href^="#"]').each(function () {
-        target = $($(this).attr('href'));
-        target.data('top', target.offset().top).data('height', target.height());
-    });
+    function onScroll(){
+        if (!activeClass) return;
+
+        scrollPos = $(document).scrollTop();
+        elt.find('a[href^="#"]').each(function(){
+            currLink = $(this);
+            refElement = $(currLink.attr('href'));
+            
+            if(refElement.data('top')-100 <= scrollPos && refElement.data('top')+ refElement.data('height')+100 > scrollPos){
+                currLink.parent().addClass('active').siblings().removeClass('active');
+            }else{
+                currLink.parent().removeClass('active');
+            }
+        });
+    }
+
+
+    elt.on('click', 'a', scrollTo);
+    setData();
+
 
     $(window).on('resize', throttle(function(){
-        requestAnimFrame(onResize);
+        requestAnimFrame(setData);
     }, 60));
-
 
     $(document).on('scroll', throttle(function(){
         requestAnimFrame(onScroll);
