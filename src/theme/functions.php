ba2daa41cb39a280fa75c55235415fb0badd7039
Elisabeth Hamel <elisabeth@stereosuper.fr>
2017-05-30 15:22:19 +0200

    :art: acf new tags improvement

--- src/theme/functions.php
+++ src/theme/functions.php
@@ -386,6 +386,8 @@ if( function_exists('acf_add_options_page') ){
 
 // Add tags in acf fields
 function beezup_acf_load_value( $value, $field, $post_id ){
+    if( is_admin() ) return $value;
+    
     $value = str_replace( '[small]', '<span class="small">', str_replace( '[/small]', '</span>', $value ) );
     $value = str_replace( '[blue]', '<span class="blue">', str_replace( '[/blue]', '</span>', $value ) );
     return $value;
