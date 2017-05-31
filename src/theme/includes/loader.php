ec53b6685a929fba4c65c5f23b18e6d20fb19afc
Elisabeth Hamel <elisabeth@stereosuper.fr>
2017-05-31 20:03:05 +0200

    :art: loader

---
+++ src/theme/includes/loader.php
@@ -0,0 +1,9 @@
+<svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' viewBox='0 0 38 15' id='loader' class='loader'>
+    <circle cx='18' cy='13' r='2'/>
+	<circle cx='10' cy='8' r='1'/>
+	<circle cx='11' cy='1' r='1'/>
+	<circle cx='31' cy='12' r='1'/>
+	<circle cx='25' cy='5' r='2'/>
+	<circle cx='1' cy='8' r='1'/>
+	<circle cx='37' cy='4' r='1'/>
+</svg>
