7c45c683b07b03d273b3d8770e72c60a61923143
Elisabeth Hamel <elisabeth@stereosuper.fr>
2017-06-07 15:38:05 +0200

    :art: blog share

--- src/js/sticky.js
+++ src/js/sticky.js
@@ -6,81 +6,73 @@ var throttle = require('./throttle.js');
 module.exports = function(stickyElt, givenPosition, unit = 'px', updateHeightOnScroll = false, wrapper = true){
     if(!stickyElt.length) return;
 
-    var position,
-        eltHeight,
-        posTop;
+    var position, eltHeight, posTop;
     var windowHeight = $(window).height(); 
     var scrollTop = $(document).scrollTop();
     var wrapperSticky = stickyElt.closest('.wrapper-sticky');
 
-    function checkWindowHeight() {
+
+    function checkWindowHeight(){
         windowHeight = $(window).height();
-        if (unit === 'vh') {
+        if(unit === 'vh'){
             eltHeight = stickyElt.outerHeight();
             position = windowHeight / (100/givenPosition) - eltHeight/2;
-        } else {
+        }else{
             position = givenPosition;
         }
     }
     
-    function scrollHandler() {
+    function scrollHandler(){
+        scrollTop = $(document).scrollTop();
+
         if(updateHeightOnScroll && stickyElt.hasClass('sticky')){
             stickyElt.data('height', stickyElt.outerHeight());
         }
-        scrollTop = $(document).scrollTop();
-        if (stickyElt.data('initialPos') === 'auto'){
-            posTop = 0;
-        } else{
-            posTop = parseFloat(stickyElt.data('initialPos'), 10);
-        }
+
+        posTop = stickyElt.data('initialPos') === 'auto' ? 0 : parseFloat(stickyElt.data('initialPos'), 10);
          
-        if (scrollTop >= stickyElt.data('offsetTop') - position + posTop) {
+        if(scrollTop >= stickyElt.data('offsetTop') - position + posTop){
             stickyElt.addClass('sticky').css('top', position+'px');
-            if (scrollTop + position + stickyElt.data('height') >= stickyElt.data('offsetBottom')) {
+            if(scrollTop + position + stickyElt.data('height') >= stickyElt.data('offsetBottom')){
                 stickyElt.removeClass('sticky').addClass('sticky-stuck').css({'top': 'auto', 'bottom': '0'});
-            }else {
+            }else{
                 stickyElt.addClass('sticky').removeClass('sticky-stuck').css({ 'top': position + 'px', 'bottom': '' });
             }
-        }else {
+        }else{
             stickyElt.removeClass('sticky').css('top', stickyElt.data('initialPos'));
         }
     }
 
-    function resizeHandler() {
+    function resizeHandler(){
         checkWindowHeight();
-        // scrollTop = $(document).scrollTop();
-        // console.log(scrollTop);
 
-        if (wrapper) {
+        if(wrapper){
             stickyElt.data({
                 'offsetTop': wrapperSticky.offset().top,
             });
-            console.log('wrap');
-        } else {
+        }else{
             stickyElt.data({
                 'offsetTop': stickyElt.offset().top,
             });
-            console.log('el');
         }
+
         stickyElt.data({
             'offsetBottom': wrapperSticky.offset().top + wrapperSticky.outerHeight(),
             'height': stickyElt.outerHeight()
         });
+
         scrollHandler();
     }
 
 
-
-    if (wrapper) {
+    if(wrapper){
         stickyElt.data({
             'offsetTop': wrapperSticky.offset().top
         });
-        console.log('wrap');
-    } else {
+    }else{
         stickyElt.data({
             'offsetTop': stickyElt.offset().top
         });
-        console.log('el');
     }
 
     stickyElt.data({
@@ -88,9 +80,6 @@ module.exports = function(stickyElt, givenPosition, unit = 'px', updateHeightOnS
         'offsetBottom': wrapperSticky.offset().top + wrapperSticky.outerHeight(),
         'height': stickyElt.outerHeight()
     });
-
-    console.log(stickyElt.data('initialPos'));
-
 
     checkWindowHeight();
 
