9f6630c551a4a1a653ca5757f01cba2a179e05dd
ClementLmn <clementlemoine.cl@gmail.com>
2017-06-15 11:31:06 +0200

    :art: tarif table responsive + less column

--- dest/wp-content/themes/beezup/tarifs.php
+++ dest/wp-content/themes/beezup/tarifs.php
@@ -26,7 +26,15 @@ get_header(); ?>
         </div>
 
     </section>
-    <div class='tarifs'>
+
+    <?php if( have_rows('offers') ){
+        $nbrOff = 0;
+        while( have_rows('offers') ){ the_row(); 
+            $nbrOff++;
+        }
+    } ?>
+
+    <div class='tarifs column-<?php echo $nbrOff;?>'>
         <div class='wrapper-sticky container-tarifs'>
             <div>
                 <div id='tarifHeader' class='tarif-header'>
@@ -34,29 +42,29 @@ get_header(); ?>
                         <div class='container-slider'>
                             <span><?php the_field('price'); ?></span>
                             <ul class='slider'>
-                                <li><button type='button'><?php _e('1 month', 'beezup'); ?></button></li>
-                                <li><button type='button'><?php _e('3 months', 'beezup'); ?></button></li>
-                                <li><button type='button'><?php _e('6 months', 'beezup'); ?></button></li>
-                                <li class='selected'><button type='button'><?php _e('1 year', 'beezup'); ?></button></li>
+                                <li class='js-price'><button class='js-btnPrice' data-price='price1' type='button'><?php _e('1 month', 'beezup'); ?></button></li>
+                                <li class='js-price'><button class='js-btnPrice' data-price='price2' type='button'><?php _e('3 months', 'beezup'); ?></button></li>
+                                <li class='js-price'><button class='js-btnPrice' data-price='price3' type='button'><?php _e('6 months', 'beezup'); ?></button></li>
+                                <li class='js-price selected'><button class='js-btnPrice' data-price='price4' type='button'><?php _e('1 year', 'beezup'); ?></button></li>
                             </ul>
                         </div>
 
                         <?php if( have_rows('offers') ){ ?>
-                        <div class='offers'>
+                        <div id='tarifOffers' class='offers'>
                             <?php while( have_rows('offers') ){ the_row(); ?>
                             <div class='offer <?php echo strtolower(get_sub_field("name")); ?>'>
                                 <h2><?php the_sub_field('name'); ?></h2>
                                 <div class='offer-spec'>
-                                    <span class='price hidden'>
+                                    <span class='js-fieldprice1 price hidden'>
                                         <?php the_sub_field('price1'); ?>
                                     </span>
-                                    <span class='price hidden'>
+                                    <span class='js-fieldprice2 price hidden'>
                                         <?php the_sub_field('price2'); ?>
                                     </span>
-                                    <span class='price hidden'>
+                                    <span class='js-fieldprice3 price hidden'>
                                         <?php the_sub_field('price3'); ?>
                                     </span>
-                                    <span class='price'>
+                                    <span class='js-fieldprice4 price'>
                                         <?php the_sub_field('price4'); ?>
                                     </span>
 
@@ -76,6 +84,15 @@ get_header(); ?>
                 <section class='container tarif-body'>
                             <?php if( have_rows('featuresSection') ){ ?>
                             <div class='wrapper-tarif-content'>
+
+                                <?php if( have_rows('offers') ){ ?>
+                                <div class='color-column'>
+                                    <?php while( have_rows('offers') ){ the_row(); ?>
+                                    <div class='<?php echo strtolower(get_sub_field("name")); ?>'></div>
+                                    <?php } ?>
+                                </div>
+                                <?php } ?>
+
                                 <ul class='tarif-content'>
                                     <?php while( have_rows('featuresSection') ){ the_row(); ?>
                                     <li class='section-feature'><ul>
@@ -101,8 +118,6 @@ get_header(); ?>
                                                                 }
                                                             }elseif( get_sub_field('check') ){
                                                                 echo '<svg class="icon icon-check"><use xlink:href="#icon-check"></use></svg>';
-                                                            }else{
-                                                                echo '';
                                                             }
                                                             ?>
                                                         </span>
