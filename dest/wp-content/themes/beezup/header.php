a02b2481800a10820ded756e4b967934f600ddf1
Elisabeth Hamel <elisabeth@stereosuper.fr>
2017-06-23 17:33:23 +0200

    :art: fix #33 favicon

--- dest/wp-content/themes/beezup/header.php
+++ dest/wp-content/themes/beezup/header.php
@@ -10,6 +10,15 @@
 		<link rel='alternate' type='application/rss+xml' title='BeezUP Feed' href='<?php bloginfo('rss2_url'); ?>'>
 		<link rel='alternate' href='<?php echo site_url(); ?>' hreflang='x-default'>
 
+		<link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png'>
+		<link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png'>
+		<link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png'>
+		<link rel='manifest' href='/manifest.json'>
+		<link rel='mask-icon' href='/safari-pinned-tab.svg' color='#00a0f0'>
+		<meta name='apple-mobile-web-app-title' content='BeezUP'>
+		<meta name='application-name' content='BeezUP'>
+		<meta name='theme-color' content='#fff'>
+
 		<?php wp_head(); ?>
 
 		<!-- Calendly style -->
