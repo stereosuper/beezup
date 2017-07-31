a16cbdacc4afe17f41e5a9e94d68b314fef0d4d7
ClementLmn <clementlemoine.cl@gmail.com>
2017-07-31 14:28:41 +0200

    :art: Landing SEO

---
+++ dest/wp-content/themes/beezup/landing.php
@@ -0,0 +1,39 @@
+<?php 
+/*
+Template Name: Landing Page
+*/
+get_header(); ?>
+
+<?php if ( have_posts() ) : the_post(); ?>
+
+    <section class='container landing'>
+        <h1 class='page-title'>
+            <?php get_field('title') ? the_field('title') : the_title(); ?>
+        </h1>
+        <div class='landing-content'>
+            <div class='landing-txt'>
+                <?php the_field('content'); ?>
+            </div>
+            <aside>
+                <div>
+                    <h3><?php the_field('form_title'); ?></h3>
+                    <?php get_template_part( 'includes/sib-form' ); ?>
+                </div>
+                <div>
+                    <h3 class='word-title'><?php the_field('word_title'); ?></h3>
+                    <?php if( have_rows('keywords') ){ ?>
+                        <ul class='landing-words'>
+                            <?php while ( have_rows('keywords') ) { the_row(); ?>
+                                <li><?php the_sub_field('word');?></li>
+                            <?php } ?>
+                        </ul>
+                    <?php } ?>
+                </div>
+            </aside>
+        </div>
+    </section>
+
+<?php endif; ?>
+
+
+<?php get_footer(); ?>
