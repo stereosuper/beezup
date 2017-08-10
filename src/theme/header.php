0bc0671c244797137eb894d2c7fdc5bb4e1846a9
Elisabeth Hamel <elisabeth@stereosuper.fr>
2017-08-10 13:39:26 +0200

    :art: add google tag manager

--- src/theme/header.php
+++ src/theme/header.php
@@ -28,6 +28,16 @@
 
 		<script>document.getElementsByTagName('html')[0].className = 'js';</script>
 
+		<!-- Google Tag Manager -->
+		<script>
+			(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+			new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+			j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+			'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+			})(window,document,'script','dataLayer','GTM-PMVMLQM');
+		</script>
+		<!-- End Google Tag Manager -->
+
 		<?php if( get_field("ga", "options") ){ ?>
 			<script>
 				(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
@@ -38,6 +48,12 @@
 	</head>
 
 	<body <?php body_class('wrapper-sticky'); ?>>
+
+		<!-- Google Tag Manager (noscript) -->
+		<noscript>
+			<iframe src='https://www.googletagmanager.com/ns.html?id=GTM-PMVMLQM' height='0' width='0' style='display:none;visibility:hidden'></iframe>
+		</noscript>
+		<!-- End Google Tag Manager (noscript) -->
 
 		<div class='wrapper'>
 
