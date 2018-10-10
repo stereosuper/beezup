ce33ab416e4fec2f799f53902575ca6223bf171d
Alphability <albanmezino@gmail.com>
2018-10-10 10:06:17 +0200

    🎨 GTM & GA iso (prod and local)
    Signed-off-by: Alphability <albanmezino@gmail.com>

--- dest/wp-content/themes/beezup/header.php
+++ dest/wp-content/themes/beezup/header.php
@@ -31,30 +31,22 @@
 		<script>document.getElementsByTagName('html')[0].className = 'js';</script>
 
 		<!-- Google Tag Manager -->
-		<script>
-			(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-			new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-			j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-			'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-			})(window,document,'script','dataLayer','GTM-TSMVR9W');
-		</script>
+		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+		j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+		'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+		})(window,document,'script','dataLayer','GTM-TSMVR9W');</script>
 		<!-- End Google Tag Manager -->
 
-		<?php if( get_field("ga", "options") ){ ?>
-			<script>
-				(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-				ga('create', '<?php the_field("ga", "options") ?>', 'auto');
-				ga('send', 'pageview');
-			</script>
-		<?php } ?>
+		<?php // if( get_field("ga", "options") ): ?>
+		<?php // endif; ?>
 	</head>
 
 	<body <?php body_class('wrapper-sticky'); ?>>
 
 		<!-- Google Tag Manager (noscript) -->
-		<noscript>
-			<iframe src='https://www.googletagmanager.com/ns.html?id=GTM-TSMVR9W' height='0' width='0' style='display:none;visibility:hidden'></iframe>
-		</noscript>
+		<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TSMVR9W"
+		height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 		<!-- End Google Tag Manager (noscript) -->
 
 		<div class='wrapper'>
